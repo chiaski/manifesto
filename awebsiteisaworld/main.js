a947304f87ee91dc52b127e8d96508f67e275136
Chia Amisola <chiamisola@gmail.com>
2023-02-07 11:23:04 -0500

    add all

--- awebsiteisaworld/main.js
+++ awebsiteisaworld/main.js
@@ -1,7 +1,72 @@
+function randInt(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 $("#wrapper .m").hover(function(){
   
   let v = $(this).attr("isa");
   
+  console.log(v);
+  
   $(".isa").html(v.toUpperCase());
   
 });
+
+
+var viewmode = "box";
+
+$("#websiteisa a").click(function(){
+
+  $("#websiteisa a").removeAttr("active");
+  let v = $(this).attr("type");
+  $(this).attr("active", "")
+  console.log( v );
+  
+  viewmode = v;
+  
+  
+  if( viewmode == "list" ){
+    
+    
+    
+  }
+  
+  if( viewmode == "popup" ){
+    
+    $("#wrapper .m p").css("display", "none")
+    
+  }
+  
+});
+
+$("#wrapper .m").click(function(){
+  
+  
+  
+  if(viewmode == "popup"){
+    
+    confirm( $(this).find("p:first-of-type").text() );
+    
+    $(this).find(".example").each(function(i, obj){
+      
+      let _top = randInt(0, screen.availHeight - 150);
+      let _left = randInt(0, screen.availWidth - 300);
+      
+      console.log( obj );
+      
+      window.open( $(obj).attr("href"), "", "top="+_top+",left=" + _left + ",width=500, height=250, location=no,toolbar=no,menubar=no,popup" )
+    });
+    
+    
+  }
+  
+  
+})
+
+$(window).on('scroll', function() {
+  
+  if(viewmode !== "box") return;
+  
+  $("#wrapper.box").css("left", -$(window).scrollTop());
+});
+
---
+++ awebsiteisaworld/popup.js
@@ -0,0 +1,8 @@
+const websiteisa = {
+  
+  "world": "Every website is a world. A website begins and comes to be, just as worlds do. Every website is a specific contextual environment and vessel shaped by its creator and then dwelled in. Every website is a world. Every website is a totality, a body that constitutes its own entities and stories, evoking a bordered place. A website is in constant relation to other websites around it. A website is held together by human activity and belief, positing a future and an environment within its premises. A website is a world. We are creators.",
+  
+  "material": "Any world can be reduced into some component breakdown, as a website can be reduced to a bundle of HTML, CSS, JS, and other accompanying assets. Any thing that can be reduced to a material must have an essence beyond these components — process, structure, and frameworks matter, but how these materials come to be in the world and our understanding of them alter our relationships with websites. A website as a bundle of files is often a forgotten factor."
+  
+  
+}
